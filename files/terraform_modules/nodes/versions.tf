e189d6853b459eff00af6eee687116ff606ddf0b
PariyaAshok <ashok.pariya@oneconvergence.com>
2024-02-22 16:44:16 +0530

    add ocp 4.14 support

--- files/terraform_modules/nodes/versions.tf
+++ files/terraform_modules/nodes/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     ignition = {
       source = "community-terraform-providers/ignition"
+      version = "2.4.0"
     }
     vsphere = {
       source = "hashicorp/vsphere"
